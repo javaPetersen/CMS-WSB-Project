5071b587e85f7ee022bc4408e163d12231862ed7
javaPetersen <perlinskipiotr@wp.pl>
2024-03-24 19:14:09 +0100

    Ostylowanie ekranu logowania na frontendzie

--- frontend/cms-angular/src/app/app.module.ts
+++ frontend/cms-angular/src/app/app.module.ts
@@ -4,15 +4,22 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {DemoComponent} from "./components/demo/demo.component";
+import {LoginComponent} from "./components/login/login.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RegistrationComponent} from "./components/registration/registration.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
